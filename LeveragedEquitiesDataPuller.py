a4fea1a2640ec54bfe21fa59539dff29ded0fc7f
Tony Lu <konigderspiele@live.com>
2023-02-18 02:40:27 -0600

    looping over dates first since the code will run for a long time

--- LeveragedEquitiesDataPuller.py
+++ LeveragedEquitiesDataPuller.py
@@ -13,22 +13,24 @@ baseUrl = "https://api.polygon.io/v1/open-close/"
 
 tickerList = ["SOXX/", "SOXL/", "SOXS/", "QQQ/", "TQQQ/", "SQQQ/", "FNGS/", "FNGD/", "FNGU/"]
 
+for ticker in tickerList:
+    os.makedirs(ticker, exist_ok=True)
+
 params = {
     'apiKey': 'YOUR_API_KEY',
     'adjusted': 'true'
 }
 
-for ticker in tickerList:
-    specificUrl = urljoin(baseUrl, ticker)
-    while iterDate < todayDate:
+while iterDate < todayDate:
+    for ticker in tickerList:
+        specificUrl = urljoin(baseUrl, ticker)
         if iterDate.isoweekday() in set((6, 7)):
             iterDate += relativedelta.relativedelta(days=8 - iterDate.isoweekday())
         specificUrl = urljoin(specificUrl, iterDate.strftime('%Y-%m-%d'))
         response = requests.get(specificUrl, params=params)
         if response.ok:
             json_object = json.dumps(response.json(), indent=4)
-            os.makedirs(ticker, exist_ok=True)
             with open(ticker + iterDate.strftime('%Y-%m-%d'), "w") as fileWriter:
                 fileWriter.write(json_object)
-        iterDate += relativedelta.relativedelta(days=1)
         time.sleep(12.5)
+    iterDate += relativedelta.relativedelta(days=1)
