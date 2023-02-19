fed8ef03aa1fc98870229b3f830069bbee985016
Tony Lu <konigderspiele@live.com>
2023-02-19 06:50:30 -0600

    fixing bugs with not updating the closing price

--- LeveragedEquitiesDataAnalyzer.py
+++ LeveragedEquitiesDataAnalyzer.py
@@ -27,27 +27,29 @@ returnPercentDict = {
 }
 
 for ticker in tickerList:
+    numberHighsBelowOpens = 0
     fileList = os.listdir(ticker)
     previousClose = -1
     with open(ticker + fileList[0], "r") as fileReader:
         data = json.load(fileReader)
-        previousClose = data['close']
+        previousClose = data["close"]
     fileList.pop(0)
     for file in fileList:
         with open(ticker + file, "r") as fileReader:
             data = json.load(fileReader)
-            if data['preMarket'] >= previousClose:
+            nextPreviousClose = data["close"]
+            if data["preMarket"] >= previousClose:
                 with open(upTickerDict[ticker] + file, "r") as leveragedFileReader:
                     data = json.load(leveragedFileReader)
-                    returnAmount = data["close"] - data["open"]
-                    returnAmountDict[ticker].append(returnAmount)
-                    returnPercentDict[ticker].append(1 + returnAmount / data["open"])
             else:
                 with open(downTickerDict[ticker] + file, "r") as leveragedFileReader:
                     data = json.load(leveragedFileReader)
-                    returnAmount = data["close"] - data["open"]
-                    returnAmountDict[ticker].append(returnAmount)
-                    returnPercentDict[ticker].append(1 + returnAmount / data["open"])
+            returnAmount = data["close"] - data["open"]
+            returnAmountDict[ticker].append(returnAmount)
+            returnPercentDict[ticker].append(1 + returnAmount / data["open"])
+            previousClose = nextPreviousClose
 
 for ticker, list in returnPercentDict.items():
+    print(ticker)
     print(reduce(operator.mul, list, 1))
+    print(sum(list) / len(list))
