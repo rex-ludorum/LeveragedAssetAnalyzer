32e2ca88449a5db59bc4fbd26ad42c18e02656f9
Tony Lu <konigderspiele@live.com>
2023-03-20 05:42:26 -0500

    pulling most recent month of data

--- LeveragedEquitiesDataPuller.py
+++ LeveragedEquitiesDataPuller.py
@@ -8,15 +8,15 @@ from requests.compat import urljoin
 
 todayDate = datetime.datetime.now().date()
 #iterDate = todayDate - relativedelta.relativedelta(years=2)
-iterDate = datetime.datetime.strptime("2021-02-24", "%Y-%m-%d").date()
+iterDate = datetime.datetime.strptime("2023-02-17", "%Y-%m-%d").date()
 
 baseUrl = "https://api.polygon.io/v1/open-close/"
 
-#tickerList = ["SOXX/", "SOXL/", "SOXS/", "QQQ/", "TQQQ/", "SQQQ/", "FNGS/", "FNGU/", "FNGD/"]
-tickerList = ["SPY/", "SPXL/", "SPXS/", "XLF/", "FAS/", "FAZ/", "VGT/", "TECL/", "TECS/", "VTWO/", "TNA/", "TZA/", "XBI/", "LABU/",
-    "LABD/", "TLT/", "TMF/", "TMV/", "DIA/", "UDOW/", "SDOW/", "FXI/", "YINN/", "YANG/", "FDN/", "WEBL/", "WEBS/", "EEM/", "EDC/",
-    "EDZ/", "SPHB/", "HIBL/", "HIBS/", "IYR/", "DRN/", "DRV/", "MDY/", "UMDD/", "SMDD/", "IEF/", "TYD/", "TYO/", "BNKU/", "BNKD/",
-    "BULZ/", "BERZ/", "GDX/", "GDXJ/", "GDXU/", "GDXD/", "XLE/", "NRGU/", "NRGD/", "IEO/", "OILU/", "OILD/"]
+tickerList = ["SOXX/", "SOXL/", "SOXS/", "QQQ/", "TQQQ/", "SQQQ/", "FNGS/", "FNGU/", "FNGD/", "SPY/", "SPXL/", "SPXS/", "XLF/", "FAS/",
+    "FAZ/", "VGT/", "TECL/", "TECS/", "VTWO/", "TNA/", "TZA/", "XBI/", "LABU/", "LABD/", "TLT/", "TMF/", "TMV/", "DIA/", "UDOW/",
+    "SDOW/", "FXI/", "YINN/", "YANG/", "FDN/", "WEBL/", "WEBS/", "EEM/", "EDC/", "EDZ/", "SPHB/", "HIBL/", "HIBS/", "IYR/", "DRN/",
+    "DRV/", "MDY/", "UMDD/", "SMDD/", "IEF/", "TYD/", "TYO/", "BNKU/", "BNKD/", "BULZ/", "BERZ/", "GDX/", "GDXJ/", "GDXU/", "GDXD/",
+    "XLE/", "NRGU/", "NRGD/", "IEO/", "OILU/", "OILD/"]
 
 for ticker in tickerList:
     os.makedirs(ticker, exist_ok=True)
